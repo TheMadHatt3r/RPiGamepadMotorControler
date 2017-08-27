59d87f5846faa21e7082014ed8d507cdc98f5f7a
ctmrocks2435 <ctmrocks2435@yahoo.com>
2017-08-27 14:19:31 -0500

    Mostly Working Example
    Lacks safe serial close on Ctrl+C.

--- SoftwareC++/software.cc
+++ SoftwareC++/software.cc
@@ -17,9 +17,16 @@
 #include <string.h>
 
 
+#define STICK_ZERO_THRESH 800
+#define STICK_ONE_THRESH  31000
+#define LEFT 1
+#define RIGHT 4
+
 int openSerialPort(void);
 void closeSerialPort(int sfd);
 void setupSerialPort(int sfd);
+int filterScaleInput(JoystickEvent event);
+void setMotorSpeed(int side, char speed, int sfd);
 
 
 
@@ -41,7 +48,7 @@ int main(int argc, char** argv)
         exit(1);
     setupSerialPort(sfd);
     //Close since I'm just testing.
-    closeSerialPort(sfd);
+    //closeSerialPort(sfd);
     
 
     while (true)
@@ -53,21 +60,87 @@ int main(int argc, char** argv)
     JoystickEvent event;
     if (joystick.sample(&event))
     {
-        if (event.isButton())
+        if (event.isAxis())
         {
-            printf("Button %u is %s\n",
-            event.number,
-            event.value == 0 ? "up" : "down");
-        }
-        else if (event.isAxis())
-        {
-            printf("Axis %u is at position %d\n", event.number, event.value);
+            //printf("Axis %u is at position %d\n", event.number, event.value);
+            int val = filterScaleInput(event);
+            if (val != -100)
+            {
+                //printf("Axis %u is at position %d\n", event.number, val);
+                if (event.number == LEFT)
+                    setMotorSpeed(LEFT, val, sfd);
+                else if (event.number == RIGHT)
+                    setMotorSpeed(RIGHT, val, sfd);
+                
+            }
         }
     }
 
     }
-    }
+}
 
+
+/**********************************************
+* Function to set proper offsets for MCU.
+* Sends to serial interface.
+* Accepts -64 to 64 char. and LEFT/RIGHT for side.
+* Returns -100 on other axis.
+*
+* Motor 1: 1=FullRev 64=Stop 127=FullFwd
+* Motor 2: 128=FullRev 192=Stop 255=FullFwd
+* Sending 0 will stop both motors.
+*
+***********************************************/
+void setMotorSpeed(int side, char speed, int sfd)
+{    
+    unsigned char buf[2];
+    buf[0] = 0;
+    
+    if(side==LEFT)
+    {
+        speed = speed + 64;
+        if (speed == 0)
+            speed = 1; //Left has 1 less precision.
+        printf("Left:%d\n",speed);
+        buf[0] = speed;
+        write(sfd, &buf, 1);
+    }
+    else
+    {
+        speed = speed + 192;
+        printf("RIGHT:%d\n",speed);
+        buf[0] = speed;
+        write(sfd, &buf, 1);
+    }
+    
+        
+}
+
+/**********************************************
+* Function to scale and filter joystick input.
+* Returns -64 to 64 int.
+* Returns -100 on other axis.
+***********************************************/
+int filterScaleInput(JoystickEvent event)
+{
+    if ((event.number == 1) || (event.number == 4))
+    {
+        int val = event.value;
+        if ((event.value > -1*STICK_ZERO_THRESH) && (event.value < STICK_ZERO_THRESH))
+            val = 0;
+        else if (event.value > STICK_ONE_THRESH)
+            val = 32767;
+        else if (event.value < -1*STICK_ONE_THRESH)
+            val = -32768;
+        val = val/512;
+        return val;
+    }
+    return -100;
+}
+
+/**********************************************
+* Open serial port and returns pointer to it.
+***********************************************/
 int openSerialPort()
 {
     int sfd = open("/dev/serial0", O_RDWR | O_NOCTTY ); 
@@ -79,18 +152,35 @@ int openSerialPort()
     return sfd;
 }
 
+/**********************************************
+* Accepts serial port pointer and configures it.
+***********************************************/
 void setupSerialPort(int sfd)
 {
-    struct termios options;
-    tcgetattr(sfd, &options);
-    cfsetspeed(&options, B9600);
-    options.c_cflag &= ~CSTOPB;
-    options.c_cflag |= CLOCAL;
-    options.c_cflag |= CREAD;
-    cfmakeraw(&options);
+    struct termios options;  // Terminal options
+    
+    fcntl(sfd, F_SETFL);                 // Configure port reading
+    tcgetattr(sfd, &options);            // Get the current options for the port
+    cfsetispeed(&options, B9600);     // Set the baud rates to 230400
+    cfsetospeed(&options, B9600);
+
+    options.c_cflag |= (CLOCAL | CREAD);    // Enable the receiver and set local mode
+    options.c_cflag &= ~PARENB;             // No parity bit
+    options.c_cflag &= ~CSTOPB;             // 1 stop bit
+    options.c_cflag &= ~CSIZE;              // Mask data size
+    options.c_cflag |=  CS8;                // Select 8 data bits
+    options.c_cflag &= ~CRTSCTS;            // Disable hardware flow control  
+
+    // Enable data to be processed as raw input
+    options.c_lflag &= ~(ICANON | ECHO | ISIG);
+
+    // Set the new attributes
     tcsetattr(sfd, TCSANOW, &options);
 }
 
+/**********************************************
+* Closes serial port given pointer to it.
+***********************************************/
 void closeSerialPort(int sfd)
 {
     if (sfd != -1)
